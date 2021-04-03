5f055b049710ed2a90ef1487fa65f96ee342074e
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-04-04 00:40:35 +0300

    Добавляет условие 6 задачи

--- js/task-06.js
+++ js/task-06.js
@@ -4,3 +4,25 @@
 Если введено подходящее количество, то border инпута становится зеленым, 
 если неправильное - красным. */
 
+const input = document.querySelector('#validation-input');
+console.log(+input.dataset.length);
+
+input.addEventListener('input', inputSymbol);
+
+function inputSymbol(event) {
+    input.textContent = event.currentTarget.value;
+    // console.log(input.textContent.length)
+    if (input.textContent.length < input.dataset.length) {
+        input.classList.add('.invalid');
+    }
+}
+
+// input.addEventListener('blur', onInputBlur);
+
+// function onInputBlur(event) {
+//     if (+input.dataset.length > 6) {
+//         input.classList.add('.valid');
+//     } else {
+//         input.classList.add('.invalid');
+//     }
+// }
