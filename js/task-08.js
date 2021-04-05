74b2552fc89e361ec67eaebdf516714e55989333
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-04-06 00:49:32 +0300

    Добавляет решение 8 задачи

--- js/task-08.js
+++ js/task-08.js
@@ -22,16 +22,15 @@ const boxes = document.querySelector('#boxes');
 createElemBtn.addEventListener('click', createBoxes);
 
 function createBoxes() {
-    const min = 0;
-    const max = 256;
-    let red = Math.floor(Math.random() * (max - min) + min);
-    let green = Math.floor(Math.random() * (max - min) + min);
-    let blue = Math.floor(Math.random() * (max - min) + min);
     const size = 30;
-
-    for (let i = 0; i < input.value; i +=1) {
+    
+    for (let i = 0; i < input.value; i += 1) {   
+    const max = 256;
+    let red = Math.floor(Math.random() * max);
+    let green = Math.floor(Math.random() * max);
+        let blue = Math.floor(Math.random() * max);
+        
         const box = document.createElement('div');
-
         box.classList.add('box');
         box.style.width = `${size+i*10}px`;
         box.style.height = `${size+i*10}px`;
@@ -39,7 +38,7 @@ function createBoxes() {
         box.style.marginRight = "10px";
         box.style.marginTop = "10px";
         boxes.append(box);
-    } 
+    }
 }
 
 deleteElemBtn.addEventListener('click', destroyBoxes);
