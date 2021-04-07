34e7557463f54ea7aea41dc61ab570569cb899ab
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-04-07 22:11:19 +0300

    Добавляет еще один вариант решения задачи

--- js/task-06.js
+++ js/task-06.js
@@ -13,6 +13,24 @@ function onInputBlur() {
         input.classList.add('valid');
     } else {
         input.classList.add('invalid');
-        
     }
 }
+
+// const input = document.querySelector('#validation-input');
+// input.addEventListener('focus', onInputFocus);
+// input.addEventListener('blur', onInputBlur);
+// function onInputFocus(event) {
+//   //   console.log('focus');
+// }
+// function onInputBlur(event) {
+//   // console.dir(event.currentTarget);
+//   // console.log('blur');
+//     console.log(event.currentTarget.value.length);
+    
+//     if (
+//       event.currentTarget.value.length >= +event.currentTarget.getAttribute('data-length')) {
+//     input.classList.add('valid');
+//   } else {
+//     input.classList.add('invalid');
+//   }
+// }
